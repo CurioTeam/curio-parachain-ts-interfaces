4e7d4b0df0829205dc35223b238ee112dac68b2e
Danila Alekseev <alexxxkny@mail.ru>
2023-10-23 15:46:52 +0300

    fresh dist

--- dist/definitions.js
+++ dist/definitions.js
@@ -1,3 +1,4 @@
 export { default as dex } from "./dex/definitions";
 export { default as primitives } from "./primitives/definitions";
 export { default as support } from "./support/definitions";
+export { default as staking } from "./staking/definitions";
--- dist/types.js
+++ dist/types.js
@@ -2,4 +2,5 @@
 /* eslint-disable */
 export * from './dex/types.js';
 export * from './primitives/types.js';
+export * from './staking/types.js';
 export * from './support/types.js';
