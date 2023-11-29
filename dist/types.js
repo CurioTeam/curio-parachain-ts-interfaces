73a1946e85373ff9cec385147eeb6071d4c0b10e
Danila Alekseev <alexxxkny@mail.ru>
2023-11-29 18:21:02 +0300

    src init

--- dist/types.js
+++
@@ -1,6 +0,0 @@
-// Auto-generated via `yarn polkadot-types-from-defs`, do not edit
-/* eslint-disable */
-export * from './dex/types.js';
-export * from './primitives/types.js';
-export * from './staking/types.js';
-export * from './support/types.js';
