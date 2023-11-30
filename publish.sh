92250936490da107d7d4992edc1118e5ed21c520
Danila Alekseev <alexxxkny@mail.ru>
2023-11-30 13:18:06 +0300

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,29 @@
-if [ -d build ]; then
-    git switch main
-    cp -rf build/* ./
-    git switch src
-else 
-    echo "build folder not found, run 'npm run build'"
+set -e
+
+usage="Usage: ./publish.sh -m \"commit message\""
+
+if [[ ! -d build ]]; then
+    echo "build/ folder not found, run 'npm run build'"
+    exit 1
+elif [[ "$1" != "-m" || -z "$2" ]]; then
+    echo $usage
+    exit 1
 fi
+
+git switch main
+git checkout src build/
+cp -rf build/* ./
+rm -rf build/
+git checkout src package.json
+
+if [[ -z "$(git status -s)" ]]; then
+    echo "no changes to publish"
+    exit 1
+fi
+
+git add interfaces/ index* package.json
+git commit -m "$2"
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+echo -e "${GREEN}+ Changes committed and ready to be pushed${NC}"
