c63658028b7b0cf8367ef768c0fdd808334a80f4
Danila Alekseev <alexxxkny@mail.ru>
2024-02-07 13:20:42 +0300

    add explicit import extensions to definitions.ts

--- build/interfaces/definitions.d.ts
+++ build/interfaces/definitions.d.ts
@@ -1,4 +1,4 @@
-export { default as dex } from "./dex/definitions";
-export { default as primitives } from "./primitives/definitions";
-export { default as support } from "./support/definitions";
-export { default as staking } from "./staking/definitions";
+export { default as dex } from "./dex/definitions.js";
+export { default as primitives } from "./primitives/definitions.js";
+export { default as support } from "./support/definitions.js";
+export { default as staking } from "./staking/definitions.js";
