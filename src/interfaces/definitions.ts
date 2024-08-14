2f86cf1f03b57285ab3758995c26b69e2c3755fa
Danila Alekseev <alexxxkny@mail.ru>
2024-08-14 17:11:54 +0300

    init develop branch

--- src/interfaces/definitions.ts
+++
@@ -1,4 +0,0 @@
-export {default as dex} from "./dex/definitions.js"
-export {default as primitives} from "./primitives/definitions.js"
-export {default as support} from "./support/definitions.js"
-export {default as staking} from "./staking/definitions.js"
